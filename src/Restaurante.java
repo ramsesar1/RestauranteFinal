0d361e7ab50e8b42d6f8220c27479d6e17c65ba6
ramsesar1 <ramsesa_19@alu.uabcs.mx>
2023-05-30 03:26:08 -0700

    cambio de tamaño botones back

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -304,7 +304,7 @@ public class Restaurante extends JFrame {
 		Platillos.add(lblplatillos);
 
 		JButton backplatillos = new JButton("Back");
-		backplatillos.setBounds(10, 11, 44, 29);
+		backplatillos.setBounds(10, 11, 80, 29);
 		Platillos.add(backplatillos);
 
 		backplatillos.addActionListener(new ActionListener() {
@@ -395,7 +395,7 @@ public class Restaurante extends JFrame {
                 panelplati.add(lblNewLabel_1);
 
                 JButton btnbackconsultaplat = new JButton("Back");
-                btnbackconsultaplat.setBounds(10, 11, 44, 29);
+                btnbackconsultaplat.setBounds(10, 11, 80, 29);
                 ConsultaPlatillos.add(btnbackconsultaplat);
 
 		btnbackconsultaplat.addActionListener(new ActionListener() {
@@ -478,7 +478,7 @@ public class Restaurante extends JFrame {
 		EditarPlat.add(EditarPLatillotitulo);
 
 		JButton btnbackeditarplatillos = new JButton("Back");
-		btnbackeditarplatillos.setBounds(10, 11, 44, 29);
+		btnbackeditarplatillos.setBounds(10, 11, 80, 29);
 		EditarPlat.add(btnbackeditarplatillos);
 
 		btnbackeditarplatillos.addActionListener(new ActionListener() {
@@ -563,7 +563,7 @@ public class Restaurante extends JFrame {
 
 
 		JButton btnbackcrearplatillos = new JButton("Back");
-		btnbackcrearplatillos.setBounds(10, 11, 44, 29);
+		btnbackcrearplatillos.setBounds(10, 11, 80, 29);
 		Crearplat.add(btnbackcrearplatillos);
 
 		btnbackcrearplatillos.addActionListener(new ActionListener() {
@@ -691,7 +691,7 @@ public class Restaurante extends JFrame {
 		panelcrearingre.add(lblPlatillosconingre);
 
 		JButton btnbackingredientes = new JButton("Back");
-		btnbackingredientes.setBounds(10, 11, 52, 43);
+		btnbackingredientes.setBounds(10, 11, 80, 29);
 		Platillosconingre.add(btnbackingredientes);
 
 		btnbackingredientes.addActionListener(new ActionListener() {
@@ -731,7 +731,7 @@ public class Restaurante extends JFrame {
 	        ElimPlatillos.add(platilloAEliminar);
 
 		JButton backbtneliminarplat = new JButton("Back");
-		backbtneliminarplat.setBounds(10, 11, 44, 29);
+		backbtneliminarplat.setBounds(10, 11, 80, 29);
 		ElimPlatillos.add(backbtneliminarplat);
 
 		backbtneliminarplat.addActionListener(new ActionListener() {
@@ -860,7 +860,7 @@ public class Restaurante extends JFrame {
 
 
 		JButton btnbackinventario = new JButton("Back");
-		btnbackinventario.setBounds(10, 11, 44, 29);
+		btnbackinventario.setBounds(10, 11, 80, 29);
 		Inventario.add(btnbackinventario);
 		btnbackinventario.addActionListener(new ActionListener() {
 			@Override
@@ -947,7 +947,7 @@ public class Restaurante extends JFrame {
 
 
 		JButton btnbackinvconsultar = new JButton("Back");
-		btnbackinvconsultar.setBounds(10, 11, 52, 43);
+		btnbackinvconsultar.setBounds(10, 11, 80, 29);
 		ConsultaInv.add(btnbackinvconsultar);
 
 		btnbackinvconsultar.addActionListener(new ActionListener() {
@@ -1040,7 +1040,7 @@ public class Restaurante extends JFrame {
 	        Editarinv3pnl.add(Articuloaeditarlbl);
 
 		JButton btnbackeditarinv = new JButton("Back");
-		btnbackeditarinv.setBounds(10, 11, 52, 43);
+		btnbackeditarinv.setBounds(10, 11, 80, 29);
 		EditarINv.add(btnbackeditarinv);
 		btnbackeditarinv.addActionListener(new ActionListener() {
 			@Override
@@ -1133,7 +1133,7 @@ public class Restaurante extends JFrame {
 
 
 		JButton btnbackeliminarinventario = new JButton("Back");
-		btnbackeliminarinventario.setBounds(10, 11, 52, 43);
+		btnbackeliminarinventario.setBounds(10, 11, 80, 29);
 		EliminarINv.add(btnbackeliminarinventario);
 
 		btnbackeliminarinventario.addActionListener(new ActionListener() {
@@ -1214,7 +1214,7 @@ public class Restaurante extends JFrame {
 		panel2.add(Creararticulobtn);
 
 		JButton btnbackcreararticuloinv = new JButton("Back");
-		btnbackcreararticuloinv.setBounds(10, 11, 52, 43);
+		btnbackcreararticuloinv.setBounds(10, 11, 80, 29);
 		NuevoArticulo.add(btnbackcreararticuloinv);
 
 		btnbackcreararticuloinv.addActionListener(new ActionListener() {
